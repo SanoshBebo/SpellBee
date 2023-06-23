c254f3b63b4c69a845a2916ef860a4fe99fe8a1e
SanoshBebo <sanoshbebo@gmail.com>
2023-06-23 13:38:02 +0530

    Added Functionality

--- app/casual/page.js
+++ app/casual/page.js
@@ -9,7 +9,18 @@ import React from "react";
 import { useState } from "react";
 
 export default function vocabulary() {
-  const [word, setWord] = useState(["S", "A", "N", "O", "S", "H"]);
+  const [word, setWord] = useState(["S", "_", "N", "_", "_", "H"]);
+  const solution = "SANOSH";
+
+  const checkAnswer = (final) => {
+    console.log("hello world");
+    console.log(final.join(""));
+    if (final.join("") == solution) {
+      console.log(word);
+      console.log(solution);
+      console.log("wow correct");
+    }
+  };
 
   return (
     <div className="bg-primary h-full ">
@@ -46,8 +57,8 @@ export default function vocabulary() {
             HINT 2
           </div>
         </div>
-        <div className="bg-white py-10 flex items-center justify-center flex-col rounded-2xl">
-          <div className="bg-[#FFF7DE] rounded-full p-5">
+        <div className="bg-white py-10 flex items-center justify-center flex-col rounded-2xl gap-10">
+          <div className="bg-[#FFF7DE] rounded-full p-5 ">
             <div className="bg-[#FFEDB9] rounded-full p-5">
               <div className="bg-[#F7B807] rounded-full p-7 hover:cursor-pointer">
                 <Image src={volume} height={50} width={50}></Image>
@@ -55,26 +66,51 @@ export default function vocabulary() {
             </div>
           </div>
 
-          <div>
-            {/* {
-              word.map(())
-            } */}
+          <div className="flex flex-row uppercase">
+            {word.map((item, index) => {
+              if (item == "_") {
+                return (
+                  <input
+                    className="bg-tertiary rounded-sm w-6 h-10 mx-[2px] text-white pl-2 placeholder-white "
+                    placeholder="_"
+                    key={index}
+                    onChange={(text) => {
+                      let copy = [...word];
+                      copy[index] = text.target.value;
+                      setWord(copy);
+                    }}
+                  />
+                );
+              } else {
+                return (
+                  <div
+                    className="bg-tertiary rounded-sm  w-6 h-10 mx-[2px] text-white flex items-center justify-center"
+                    key={index}
+                  >
+                    {item}
+                  </div>
+                );
+              }
+            })}
           </div>
           <div>
             <Button
               text="NEXT"
               textClassName="font-lilita py-3 px-28"
               containerClassName="mt-5"
+              onClick={() => checkAnswer(word)}
             ></Button>
           </div>
         </div>
         <div className="flex flex-col gap-2">
           <div className="font-lilita text-white text-3xl "> GAME RULES:</div>
-          <div className="">
-            Click on the volume icon to listen to the word. <br />
-            Spell the correct word by typing in the missing letters. <br />
-            Spell the correct word before the timer runs out. <br />
-            Use hints if you get stuck. (Using hints deducts points)
+          <div className="ml-5 text-[#9D3D1A] font-poppins font-medium">
+            <ul className="list-disc">
+              <li>Click on the volume icon to listen to the word.</li>
+              <li>Spell the correct word by typing in the missing letters.</li>
+              <li>Spell the correct word before the timer runs out.</li>
+              <li>Use hints if you get stuck. (Using hints deducts points)</li>
+            </ul>
           </div>
         </div>
       </div>
